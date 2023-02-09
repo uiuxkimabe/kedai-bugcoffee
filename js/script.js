8c3f2693b4f8d0e349c93c58675e95d4f2c733f0
uiuxkimabe <104246793+uiuxkimabe@users.noreply.github.com>
2023-02-09 17:02:37 +0700

    File Upload First

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const hamMenu = document.getElementById("hamMenu");
+const navbar = document.querySelector(".navbar");
+hamMenu.addEventListener("click", () => {
+  navbar.classList.toggle("showup");
+});
